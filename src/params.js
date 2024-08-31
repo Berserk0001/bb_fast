2a62a06492511433b09cdf17d483074a2e208e81
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-08-31 20:57:36 +0530

    Create params.js

---
+++ src/params.js
@@ -0,0 +1,19 @@
+"use strict";
+const DEFAULT_QUALITY = 40;
+
+function params(req, reply, done) {
+  let url = req.query.url;
+  if (!url) {
+    reply.send('bandwidth-hero-proxy');
+    return;
+  }
+
+  req.params.url = decodeURIComponent(url);
+  req.params.webp = !req.query.jpeg;
+  req.params.grayscale = req.query.bw != 0;
+  req.params.quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY;
+
+  done();
+}
+
+module.exports = params;
